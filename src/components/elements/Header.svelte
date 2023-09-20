e775ab3354c3844da2b5a7acb7d133673cc70fdf
Alexandre Nicol <nicol.alexandre1@gmail.com>
2023-09-20 08:58:29 +0000

    post demo

---
+++ src/components/elements/Header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let pageTitle: string = "default title";
+</script>
+
+<header>{pageTitle}</header>
+
+<style>
+  header {
+    height: 3rem;
+    width: 100vw;
+    margin-bottom: 1rem;
+    padding: 0.5rem;
+    background-color: var(--colour-primary);
+    color: var(--colour-white);
+  }
+</style>
